1c376da666998aeecb2c7885dfee4c80a25c36bc
Mailson <38691325+mailsonsoares@users.noreply.github.com>
2024-10-01 19:05:57 -0400

    Update BetCandidate.sol

--- BetCandidate.sol
+++ BetCandidate.sol
@@ -15,16 +15,18 @@ struct Dispute {
     string image2;
     uint total1;
     uint total2;
+    uint totalBets1;
+    uint totalBets2;
     uint winner;
 }
 
 contract BetCandidate{
-
     Dispute public dispute;
     mapping(address => Bet) public allBets;
-    address owner;
-    uint fee = 1000; // 10% na escala de 4 zeros
+    address immutable  owner;
+    uint constant fee = 1000; // 10% na escala de 4 zeros
     uint public netPrize;
+    uint public comission;
 
     constructor(){
         owner = msg.sender;
@@ -35,6 +37,8 @@ contract BetCandidate{
             image2: "http://bit.ly/4gF4mYO",
             total1: 0,
             total2: 0,
+            totalBets1: 0,
+            totalBets2: 0,
             winner: 0
         });
     }
@@ -43,6 +47,8 @@ contract BetCandidate{
         require(candidate ==  1 || candidate == 2, "Invalid Candidate");
         require(msg.value > 0, "Invalid Value");
         require(dispute.winner == 0, "Dispute Closed");
+        require(block.timestamp < 1727830800 , "Dispute Closed"); //Data limite para apostar
+        require(allBets[msg.sender].amount == 0, "User already placed a bet"); // Só aceita se o apostador não fez nenhuma aposta
 
         Bet memory newBet;
         newBet.amount = msg.value;
@@ -50,19 +56,28 @@ contract BetCandidate{
         newBet.timestamp = block.timestamp;
 
         allBets[msg.sender] = newBet;
-
+      
         candidate == 1 ? dispute.total1 += msg.value : dispute.total2 += msg.value;
+        candidate == 1 ? dispute.totalBets1 += 1 : dispute.totalBets2 += 1;
     }
 
+   
+  
     function finish(uint winner) external {
+        require(block.timestamp > 1727820000 , "Dispute Closed"); //Data mínima para encerrar
         require(msg.sender == owner, "Invalid Account Owner");
         require(winner == 1 || winner == 2, "Invalid Candidate");
         require(dispute.winner == 0, "Dispute Closed");
 
         dispute.winner = winner;
         uint grossPrize = dispute.total1 + dispute.total2;
-        uint comission = (grossPrize * fee) / 1e4;
+        comission = (grossPrize * fee) / 1e4;
         netPrize = grossPrize - comission;
+    }
+
+    function claimComission() external {// Saque da comissão
+        require(msg.sender == owner, "Invalid Account Owner");
+        require(dispute.winner != 0, "Dispute Opened");
 
         payable(owner).transfer(comission);
     }
